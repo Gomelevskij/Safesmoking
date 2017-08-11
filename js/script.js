06e2406dc3402d04d58cb14d27b4d15db8aec437
Gomelevskij <personal@gomelevskij.ru>
2017-08-11 09:03:46 +0300

    add style to section4, front cicle (css, js)

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
 
+  // Carousel to screen 3
   $(".section3-list").owlCarousel({
     items: 1
   });
+
+  // Circle to screen 3 in class 'round'
+  var Circle = function(sel){
+    var circles = document.querySelectorAll(sel);
+    [].forEach.call(circles,function(el){
+      var valEl = parseFloat(el.innerHTML);
+      valEl = valEl*408/100;
+      el.innerHTML = '<svg width="305" height="305" viewBox="0 0 150 160"><circle transform="rotate(-90)" r="65" cx="-80" cy="80" /><circle transform="rotate(-90)" style="stroke-dasharray:'+valEl+'px 408px;" r="65" cx="-80" cy="80" /></svg>';
+
+    });
+  };
+  Circle('.round');
 
 });
