8457dd5b10af4c755bce9a3d66f017cfd192497f
Gomelevskij <personal@gomelevskij.ru>
2017-08-15 16:07:25 +0300

    Section 6-7, add new carousel, image and more css

--- js/script.js
+++ js/script.js
@@ -8,13 +8,37 @@ $(document).ready(function() {
     animateOut: 'fadeOut',
     mouseDrag: false,
     singleItem: true,
-    nav: true
+    nav: true,
+    navText: false
   });
 
   owlMap.on('changed.owl.carousel', function(event) {
     var indexMap = '.map' + event.item.index;
     $(".map-icon").removeClass("active");
     $(indexMap).addClass("active");
+  });
+
+  $('.map-icon').click(function() {
+    var mapSelector = $(this).attr("map-attr");
+    owlMap.trigger('to.owl.carousel', [mapSelector, 500]);
+  });
+
+  // Carousel to screen 6
+  var owlGrafic = $(".grafic-digital");
+  owlGrafic.owlCarousel({
+    items: 1,
+    animateIn: 'fadeIn',
+    animateOut: 'fadeOut',
+    mouseDrag: false,
+    singleItem: true,
+    nav: true,
+    navText: false
+  });
+
+  owlGrafic.on('changed.owl.carousel', function(event) {
+    var indexGrafic = '.grafic' + event.item.index;
+    $(".grafic-line").removeClass("active");
+    $(indexGrafic).addClass("active");
   });
 
   // Circle to screen 3 in class 'round'
